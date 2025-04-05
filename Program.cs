54cfba700f340f26168d131c7e30526c0c7addfe
Lonewolf239 <semga05@mail.ru>
2025-04-05 03:46:25 +0300

    Fixed detection of correct location of exe file

--- Program.cs
+++ Program.cs
@@ -15,10 +15,18 @@ namespace _7DtD_HP
         {
             Mutex mutex = new Mutex(true, "7DtD_Unique_Mutex");
             if (!mutex.WaitOne(TimeSpan.Zero, true)) return;
-            if (!Directory.GetCurrentDirectory().EndsWith("7 Days To Die") || !Directory.Exists("Data\\Config") || !File.Exists(ECFile))
+            if (!Directory.GetCurrentDirectory().EndsWith("7 Days To Die") || !Directory.Exists("Data\\Config"))
             {
                 MessageBox.Show("Invalid launch location. The application must be located in the \"7 Days To Die\" game folder. Please move 7DtD_HP.exe to the correct folder and try again.",
                                 "Invalid Launch Location",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                return;
+            }
+            if (!File.Exists(ECFile))
+            {
+                MessageBox.Show("The required configuration file is missing! Please verify your game files through Steam to resolve the issue.",
+                                "Configuration File Missing",
                                 MessageBoxButtons.OK,
                                 MessageBoxIcon.Error);
                 return;
